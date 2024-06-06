9bec945e86af52a7eb859ba6b82d4153606cf102
yjglab <yjgdesign@gmail.com>
2024-06-06 21:02:30 +0900

    *app/add: 추가사항 반영
    - 데크 예약 시스템 추가.
    - 캠프 예약 시스템 추가.
    - 보안문자 입력 이전 모든 입력창에 사용자 정보 입력 및 선택 옵션 체크 완료.

---
+++ reserveCamp.js
@@ -0,0 +1,92 @@
+import { default as pt } from "puppeteer";
+
+export default async function reserveCamp() {
+  const browser = await pt.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(process.env.RSV_MAIN_PAGE);
+  await page.setViewport({ width: 600, height: 900 });
+
+  page.on("dialog", async (dialog) => {
+    await dialog.accept();
+  });
+
+  const campSltr = await page.waitForSelector(".btn-box .sbtn_2");
+  campSltr.click();
+
+  await page.waitForNavigation();
+
+  const nextMonthSltr = await page.$('button[class="next"]');
+  const getTableMonth = async () => {
+    return await (
+      await page.waitForSelector(".month")
+    )?.evaluate((el) => el.textContent);
+  };
+
+  while (!(await getTableMonth()).includes(process.env.RSV_TG_MONTH)) {
+    await nextMonthSltr.click();
+  }
+
+  const staySltr = await page.waitForSelector(
+    `#change_stay${process.env.RSV_CAMP_STAY}`
+  );
+  await staySltr.click();
+
+  const dayButtons = await page.$$(".facility_btn");
+  for (const dayButton of dayButtons) {
+    const dayButtonText = await dayButton.evaluate((el) => el.textContent);
+    if (dayButtonText.includes(process.env.RSV_TG_DAY)) {
+      if (!dayButtonText.includes("예약가능")) {
+        alert("이 일정에 예약할 수 없음");
+      } else {
+        await dayButton.click();
+      }
+      break;
+    }
+  }
+
+  await page.waitForNavigation();
+
+  const deckRsvRows = await page.$$("tr");
+  for (const row of deckRsvRows) {
+    const deckRsvRowText = await row.evaluate((el) => el.textContent);
+    if (deckRsvRowText.includes(process.env.RSV_TG_CAMP)) {
+      const deckRsvButton = await row.$(".res_btn");
+      await deckRsvButton.click();
+      break;
+    }
+  }
+
+  await page.waitForNavigation();
+
+  const agreeCheckButtons = await page.$$(".agree_check");
+  for (const agreeCheckButton of agreeCheckButtons) {
+    await agreeCheckButton.click();
+  }
+
+  const rsvUserNameInput = await page.$("#res_name");
+  await rsvUserNameInput.type(process.env.RSV_USER_NAME);
+
+  const rsvPhoneMidInput = await page.$("#tel_2");
+  await rsvPhoneMidInput.type(process.env.RSV_USER_PHONE_MID);
+
+  const rsvPhoneLstInput = await page.$("#tel_3");
+  await rsvPhoneLstInput.type(process.env.RSV_USER_PHONE_LST);
+
+  await page.select(`select[name="res_add"`, process.env.RSV_USER_CITY);
+  await page.select(`select[name="res_add_1"`, process.env.RSV_USER_LOCAL);
+
+  const cusRadioButton = await page.$("#cus_gbn_2");
+  await cusRadioButton.click();
+
+  (await page.$("#captchacode")).focus();
+
+  return;
+
+  // await browser.close();
+}
+
+// const rsvUserCountInput = await page.$("#res_qty");
+// await rsvUserCountInput.click({ clickCount: 3 });
+// await page.keyboard.press("Backspace");
+// await rsvUserCountInput.type(process.env.RSV_USER_COUNT);
